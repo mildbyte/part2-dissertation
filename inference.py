ff8e6276fabaedc6494406d1dc5f35983ff54d03
Artjoms Iskovs <ai280@cam.ac.uk>
2014-12-14 11:51:01 +0000

    Performance improvements to the theta inference procedure

--- inference.py
+++ inference.py
@@ -10,34 +10,41 @@ import scipy.stats
 from math_utils import safe_log
 
 """Sampling of the likelihood based on the variational posterior"""
-def sample_term(v_params, m_params, doc, counts, eta):
-    t1 = 0.5 * safe_log(np.linalg.det(m_params.inv_sigma))
-    t1 -= 0.5 * (np.diag(v_params.nu_sq).dot(m_params.inv_sigma)).trace()
-    lambda_mu = v_params.lambd - m_params.mu
-    t1 -= 0.5 * lambda_mu.dot(m_params.inv_sigma.dot(lambda_mu))
-    
+def sample_term(v_params, m_params, doc, counts, eta, t1):
     theta = np.exp(eta)
     theta /= sum(theta)
     
-    for (n, c) in zip(xrange(len(doc)), counts):
-        t1 += c * safe_log(np.sum(theta * m_params.beta[:, doc[n]]))
-        
+    t1 += counts.dot(safe_log(theta.dot(m_params.beta[:, doc])))
+#    
+#    for (n, c) in zip(xrange(len(doc)), counts):
+#        t1 += c * safe_log(np.sum(theta * m_params.beta[:, doc[n]]))
+#        
     t2 = np.sum(safe_log(scipy.stats.multivariate_normal.pdf(eta - v_params.lambd, np.zeros(len(eta)), np.diag(np.sqrt(v_params.nu_sq)))))
     
     return t1 - t2
 
 def sample_lhood(v_params, m_params, doc, counts):
     nsamples = 10000
+
+    t1 = 0.5 * np.linalg.slogdet(m_params.inv_sigma)[1]
+    t1 -= 0.5 * (np.diag(v_params.nu_sq).dot(m_params.inv_sigma)).trace()
+    lambda_mu = v_params.lambd - m_params.mu
+    t1 -= 0.5 * lambda_mu.dot(m_params.inv_sigma.dot(lambda_mu))
     
     def terms():
         for _ in xrange(nsamples):
             eta = np.random.multivariate_normal(v_params.lambd, np.diag(np.sqrt(v_params.nu_sq)))
-            yield sample_term(v_params, m_params, doc, counts, eta)
+            yield sample_term(v_params, m_params, doc, counts, eta, t1)
             
     return scipy.misc.logsumexp(np.array(terms())) - safe_log(nsamples)
     
 def expected_theta(v_params, m_params, doc, counts):
-    nsamples = 100
+    nsamples = 1000
+
+    t1 = 0.5 * np.linalg.slogdet(m_params.inv_sigma)[1]
+    t1 -= 0.5 * (np.diag(v_params.nu_sq).dot(m_params.inv_sigma)).trace()
+    lambda_mu = v_params.lambd - m_params.mu
+    t1 -= 0.5 * lambda_mu.dot(m_params.inv_sigma.dot(lambda_mu))
     
     def terms():
             
@@ -47,7 +54,7 @@ def expected_theta(v_params, m_params, doc, counts):
             theta = np.exp(eta)
             theta /= sum(theta)
         
-            w = sample_term(v_params, m_params, doc, counts, eta)
+            w = sample_term(v_params, m_params, doc, counts, eta, t1)
             
             yield w + safe_log(theta)
     
