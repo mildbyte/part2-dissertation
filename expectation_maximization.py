940e403dba3f586bbfdacf52091efcd8b7e6a4ee
Artjoms Iskovs <ai280@cam.ac.uk>
2014-12-15 16:18:08 +0000

    EM can now be initialized with initial model parameters (to continue inference from a certain point)

--- expectation_maximization.py
+++ expectation_maximization.py
@@ -8,6 +8,10 @@ Created on Sun Dec  7 18:02:06 2014
 import numpy as np
 from variational_inference import variational_inference, likelihood_bound
 from multiprocessing import Pool
+from math_utils import cor_mat
+
+def dsm_rmse(inf, ref):
+    return np.sqrt(np.sum(np.square(inf-ref)) / ref.size) / (np.max(ref) - np.min(ref))
 
 
 class Model():
@@ -28,9 +32,11 @@ class VIWorker:
         return variational_inference(x[0], x[1], self.m_params)   
         
 """Trains the model on the corpus with given pathway priors and returns the MLE for sigma, mu and beta."""
-def expectation_maximization(corpus, word_counts, no_pathways, pathway_priors, max_iterations=10):
-    m_params = Model(np.zeros(no_pathways), np.identity(no_pathways), np.array(pathway_priors))
-    
+def expectation_maximization(corpus, word_counts, no_pathways, pathway_priors, max_iterations=5, initial_params=None):
+    if initial_params is not None:
+        m_params = initial_params
+    else:
+        m_params = Model(np.zeros(no_pathways), np.identity(no_pathways), np.array(pathway_priors))
     
     expanded_counts = []
     for w, c in zip(corpus, word_counts):
@@ -48,7 +54,7 @@ def expectation_maximization(corpus, word_counts, no_pathways, pathway_priors, m
         print "Performing variational inference..."
 
         params = pool.map(VIWorker(m_params), zip(corpus, word_counts))
- #       params = map(VIWorker(m_params), zip(corpus, word_counts))
+        params = map(VIWorker(m_params), zip(corpus, word_counts))
         
         old_l_bound = sum([likelihood_bound(p, m_params, d, c, sum(c)) for (p, d, c) in zip(params, corpus, word_counts)])
         print "Old bound: " + str(old_l_bound)
@@ -57,7 +63,7 @@ def expectation_maximization(corpus, word_counts, no_pathways, pathway_priors, m
         sigma = np.sum([np.diag(p.nu_sq) + np.outer(p.lambd, p.lambd) for p in params], axis=0)
         sigma /= len(corpus)
         sigma -= np.outer(mu, mu)        
-        sigma += np.eye(len(sigma)) * 0.01 #Ridge on the principal diagonal to avoid singular matrices
+        sigma += np.eye(len(sigma)) * 0.00001 #Ridge on the principal diagonal to avoid singular matrices
         
         expanded_phis = [] #Sparse phi s.t. exp_phi[w, i] = phi[doc^-1[w], i] or 0 if doc^-1 is undefined at w
         for param, words in zip(params, corpus):
@@ -81,6 +87,8 @@ def expectation_maximization(corpus, word_counts, no_pathways, pathway_priors, m
         
         m_params = Model(mu, sigma, beta)
         print m_params
+        print "RMSE between inferred beta and the reference: %f" % dsm_rmse(np.array(m_params.beta), np.array(actual_beta))
+        print "RMSE between inferred topic correlations and the reference: %f" % dsm_rmse(cor_mat(m_params.sigma), cor_mat(actual_sigma))
         
         new_l_bound = sum([likelihood_bound(p, m_params, d, c, sum(c)) for (p, d, c) in zip(params, corpus, word_counts)])
         print "New bound: " + str(new_l_bound)
