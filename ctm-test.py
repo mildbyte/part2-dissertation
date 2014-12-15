424b401a02b9bdb592346344a801438ffee08268
Artjoms Iskovs <ai280@cam.ac.uk>
2014-12-15 16:17:01 +0000

    Added loading of actual pathway/drug names and the tagging of them on the graph

--- ctm-test.py
+++ ctm-test.py
@@ -7,7 +7,7 @@ Created on Thu Oct 23 10:43:11 2014
 
 from variational_inference import variational_inference
 from expectation_maximization import expectation_maximization
-from inference import expected_theta
+from inference import parallel_expected_theta
 from evaluation_tools import generate_random_corpus, document_similarity_matrix, dsm_rmse
 from math_utils import cor_mat
 
@@ -18,16 +18,21 @@ import scipy.misc
 import networkx as nx
 np.set_printoptions(precision=2, linewidth=120)
 
-def plot_correlations(sigma, threshold=0.1):
+def plot_correlations(sigma, threshold=0.1, sigma_labels=None):
+    K = sigma.shape[0]
+    if sigma_labels is None:
+        labels = {i: i for i in xrange(K)}
+    else:
+        labels = {i: sigma_labels[i] for i in xrange(K)}
+    
     corr = cor_mat(sigma)
-    K = corr.shape[0]
     
     G = nx.Graph()
     G.add_nodes_from(xrange(K))
     G.add_weighted_edges_from([(a, b, 1.0/corr[a, b]) for a in xrange(K) for b in xrange(K) if a != b and corr[a, b] > threshold])
     
     pos = nx.fruchterman_reingold_layout(G)
-    nx.draw(G, pos, with_labels=True, edge_cmap=plt.get_cmap("Blues"), alpha=0.7, node_size=500, width=5.0, labels={i: i for i in xrange(K)})
+    nx.draw(G, pos, with_labels=True, edge_cmap=plt.get_cmap("Blues"), alpha=0.7, node_size=1000, width=3.0, labels=labels)
     labels={(u,v,) : "%.2f" % (1.0/d['weight']) for u, v, d in G.edges(data=True)}
     nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
 
@@ -36,33 +41,64 @@ def plot_cdf(arr):
     cdf = np.cumsum(counts)
     cdf /= max(cdf)
     plt.plot(edges[1:], cdf)
+
+def plot_rank(theta, pathway_labels=None, prune_top=None):
+    if prune_top is None:
+        prune_top = len(theta)
+    
+    sorted_pathways = range(len(theta))
+    sorted_pathways.sort(key=lambda p: theta[p], reverse=True)
+    
+    if pathway_labels is None:
+        pathway_labels = sorted_pathways[:prune_top]
+    else:
+        pathway_labels = np.array(pathway_labels)[sorted_pathways[:prune_top]]
+    
+    plt.bar(range(prune_top), theta[sorted_pathways[:prune_top]])
+    plt.xticks(range(prune_top), pathway_labels, rotation='vertical')
     
 if __name__ == "__main__":
     drug_prune = 10
-    gene_prune = 10
-    pathway_prune = 10
+    gene_prune = 1
+    pathway_prune = 1
+
     
     drug_gene = np.loadtxt("D:\\diss-data\\gene_expression_matrix_X.txt").T    
     drug_gene = np.round(np.abs(drug_gene*100)).astype('int')
-    drug_gene = drug_gene[::drug_prune,::gene_prune]        
+    drug_gene = drug_gene[::drug_prune,::gene_prune]     
     
     doc_words = [d.nonzero()[0] for d in drug_gene]
     doc_counts = [d[d.nonzero()[0]] for d in drug_gene]
-#
-#    pathway_gene = np.loadtxt("D:\\diss-data\\gene_pathway_matrix_K.txt")[::pathway_prune,::gene_prune]
-#    priors = np.multiply(np.random.uniform(size=pathway_gene.shape), pathway_gene)
-#    #priors = np.random.uniform(size=pathway_gene.shape)
-#    #priors[priors > 0.5] = 0
-#    #priors /= 0.5
-#    
-#    print "Drugs: %d, pathways: %d, genes: %d" % (drug_gene.shape[0], pathway_gene.shape[0], drug_gene.shape[1])
-#    priors = np.array([p / sum(p) for p in priors])
-#    
-#    m_params, v_params = expectation_maximization(doc_words, doc_counts, len(priors), priors)
-    
-    ps = list(expectation_maximization(doc_words, doc_counts, len(priors), priors))
-    t = expected_theta(v_params[0], m_params, doc_words[0], doc_counts[0])
 
+    pathway_gene = np.loadtxt("D:\\diss-data\\gene_pathway_matrix_K.txt")[::pathway_prune,::gene_prune]
+    priors = np.multiply(np.random.uniform(size=pathway_gene.shape), pathway_gene)
+    #priors = np.random.uniform(size=pathway_gene.shape)
+    #priors[priors > 0.5] = 0
+    #priors /= 0.5
+#    
+    priors = np.array([p / sum(p) for p in priors])
+    print "Drugs: %d, pathways: %d, genes: %d" % (drug_gene.shape[0], pathway_gene.shape[0], drug_gene.shape[1])
+# 
+#    m_params, v_params = expectation_maximization(doc_words, doc_counts, len(priors), priors, max_iterations=10)
+    
+    import cPickle
+    m_params, v_params = cPickle.load(open("D:\\data-every-10th-drug.dat", 'rb'))
+    thetas = cPickle.load(open("D:\\thetas-every-10th-drug.dat", 'rb'))
+        
+    pathway_ids = [int(p[:-1]) for p in open("D:\\diss-data\\pathway_id.txt").readlines()][::pathway_prune]
+    drug_names = [d[:-1] for d in open("D:\\diss-data\\drug_name.txt").readlines()][1::drug_prune]
+    
+    
+#    f = open("D:\\data-every-" + str(drug_prune) + "th-drug.dat", 'wb')
+#    cPickle.dump((m_params, v_params), f)
+#    f.close()
+#    
+#    thetas = [expected_theta(v, m_params, d, c) for v, d, c in zip(v_params, doc_words, doc_counts)]
+#    f = open("D:\\thetas-every-" + str(drug_prune) + "th-drug.dat", 'wb')
+#    cPickle.dump(thetas, f)
+#    f.close()
+#    
+    
         
 #TODO:
 #manually assign the parameters (sigma, mu)
